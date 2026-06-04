7e80eda8bb94d90e9e4cf2914f27142403834b43
rernamp <renamp100@gmail.com>
2026-06-04 08:18:45 +0300

    start added work example for test mcu is work

--- Platforms/CH32V307-R1-1v1/SDKPart/User/system_ch32v30x.h
+++ Platforms/CH32V307-R1-1v1/SDKPart/User/system_ch32v30x.h
@@ -1,1 +1,21 @@
-#pragma once
+#ifndef __SYSTEM_CH32V30x_H 
+#define __SYSTEM_CH32V30x_H
+
+#ifdef __cplusplus
+ extern "C" {
+#endif 
+
+extern uint32_t SystemCoreClock;
+
+/* System_Exported_Functions */  
+extern void SystemInit(void);
+extern void SystemCoreClockUpdate(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /*__CH32V30x_SYSTEM_H */
+
+
+
